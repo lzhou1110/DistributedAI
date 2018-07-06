54e446783787b0f30077e261e0c2614bc67d0569
fallenk <fallenk_liu@163.com>
2018-07-06 19:42:26 +0800

    add forward, backprogation, calculate function

--- notebooks/A Tour of go/goMnist/src/gonn/gonn.go
+++ notebooks/A Tour of go/goMnist/src/gonn/gonn.go
@@ -2,6 +2,8 @@ package gonn
 import(
 	"math/rand"
 	"time"
+	"fmt"
+	"math"
 )
 type NeuralNetwork struct {
 	HiddenLayer			[]float64
@@ -63,7 +65,110 @@ func NewNetwork(iInputCount, iHiddenCount, iOutputCount int, iRegression bool, i
 
 	return network;
 }
-// Train 训练数据
+// genRandomIdx   N=60000
+func genRandomIdx(N int) []int {
+	A := make([]int, N);
+	for i:=0; i<N; i++ {
+		A[i] = i;
+	}
+	// randomsize
+	for i:=0; i<N; i++ {
+		j := i + int(rand.Float64() * float64(N-i));
+		A[i], A[j] = A[j], A[i];
+	}
+	return A;
+}
+// activation function
+func sigmoid(X float64) float64 {
+	return  1.0 / (1.0 + math.Pow(math.E, -float64(X)));
+}
+func dsigmoid(Y float64) float64 {
+	return Y * (1.0 - Y);
+}
+// Forward 前向传播 input = (60000,)
+func (self *NeuralNetwork) Forward(input []float64) (output []float64) {
+	if len(input) + 1 != len(self.InputLayer){
+		panic("amount of input variable doesn't match");
+	}
+	for i := 0; i < len(input); i++ {
+		self.InputLayer[i] = input[i];
+	}
+	self.InputLayer[len(self.InputLayer) - 1] = 1.0; // bias node for input layer
+	for i:= 0; i<len(self.HiddenLayer)-1; i++ {
+		sum := 0.0;
+		for j := 0; i < len(self.InputLayer); j++ {
+			sum += self.InputLayer[j] * self.WeightHidden[i][j];
+		}
+		self.HiddenLayer[i] = sigmoid(sum);
+	}
+	// HiddenLayer
+	self.HiddenLayer[len(self.HiddenLayer) - 1] = 1.0; // bias node for hidden layer
+	for i := 0; i < len(self.OutputLayer); i++ {
+		sum := 0.0;
+		for j := 0; j < len(self.HiddenLayer); j++ {
+			sum += self.HiddenLayer[j] * self.WeightOutput[i][j];
+		}
+		if self.Regression {
+			self.OutputLayer[i] = sum;
+		} else {
+			self.OutputLayer[i] = sigmoid(sum);
+		}
+	}
+	return self.OutputLayer;
+}
+//Feedback backprogation
+func (self *NeuralNetwork) Feedback(target []float64) {
+	// record ErrOutput
+	for i := 0; i < len(self.OutputLayer); i++ {
+		self.ErrOutput[i] = self.OutputLayer[i] - target[i];
+	}
+	for i := 0; i < len(self.OutputLayer) - 1; i++ {
+		err := 0.0;
+		for j := 0; j <len(self.OutputLayer); j++ {
+			if self.Regression {
+				err += self.ErrOutput[j] * self.WeightOutput[j][i];
+			} else {
+				err += self.ErrOutput[j] * self.WeightOutput[j][i] * dsigmoid(self.OutputLayer[j]);
+			}
+		}
+		self.ErrHidden[i] = err;
+	}
+
+	for i:=0; i <len(self.OutputLayer); i++ {
+		for j := 0; j <len(self.HiddenLayer); j++ {
+			change := 0.0;
+			delta := 0.0;
+			if self.Regression {
+				delta = self.ErrOutput[i];
+			} else {
+				delta = self.ErrOutput[i] * dsigmoid(self.OutputLayer[i]);
+			}
+			change = self.Rate1 * delta * self.HiddenLayer[j] + self.Rate2 * self.LastChangeOutput[i][j];
+			self.WeightOutput[i][j] -= change;
+			self.LastChangeOutput[i][j] = change;
+		}
+	}
+
+	for i := 0; i <len(self.HiddenLayer) - 1; i++ {
+		for j := 0; j < len(self.InputLayer); j++ {
+			delta := self.ErrHidden[i] * dsigmoid(self.HiddenLayer[i]);
+			change := self.Rate1 * delta * self.InputLayer[j] + self.Rate2 * self.LastChangeHidden[i][j];
+			self.WeightHidden[i][j] -= change;
+			self.LastChangeHidden[i][j] = change;
+		}
+	}
+}
+
+// 计算 loss
+func (self *NeuralNetwork) CalcError(target []float64) float64 {
+	errSum := 0.0;
+	for i := 0; i < len(self.OutputLayer); i++ {
+		err := self.OutputLayer[i] - target[i];
+		errSum += 0.5 * err * err;
+	}
+	return errSum;
+}
+// Train 训练数据，训练模型参数 inputs(60000, 784)  targets(60000, 10) iteration
 func (self *NeuralNetwork) Train(inputs [][]float64, targets [][]float64, iteration int) {
 	if len(inputs[0]) + 1 != len(self.InputLayer) {
 		panic("amount of input variable doesn't match");
@@ -72,5 +177,27 @@ func (self *NeuralNetwork) Train(inputs [][]float64, targets [][]float64, iterat
 		panic("amount of output variable doesn't match");
 	}
 	iter_flag := -1;
-	for
+	// iteration for loop
+	for i:=0; i<iteration; i++ {
+		idx_array := genRandomIdx(len(inputs));
+		cur_err := 0.0;
+		// 全体数据 inputs
+		for j:=0; j < len(inputs); j++ {
+			self.Forward(inputs[idx_array[j]]);		// Forward train
+			self.Feedback(targets[idx_array[j]]);		// backprogation
+			cur_err += self.CalcError(targets[idx_array[j]]); // caculate error
+			if (j+1) % 1000 == 0 {
+				if iter_flag != i {
+					fmt.Println("");
+					iter_flag = i;
+				}
+				fmt.Printf("iteration %vth / progress %.2f %% \r", i+1, float64(j) * 100/float64(len(inputs)));
+			}
+			if (iteration >= 10 && (i + 1) % (iteration / 10 ) == 0) || iteration < 10 {
+				fmt.Printf("\niteration %vth loss: %.5f", i+1, cur_err / float64(len(inputs)));
+			}
+
+		}
+		fmt.Println("\ndone.");
+	}
 }
